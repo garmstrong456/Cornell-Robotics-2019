b7bb156cf166f14c167b63ce572a5ca1a31182e8
Greg <garmstrong456@gmail.com>
2019-07-01 12:49:51 -0400

    origin/master

--- Hello_World/Hello_World.ino
+++ Hello_World/Hello_World.ino
@@ -1,9 +1,9 @@
 #include <M5Stack.h>
 
 void setup() {
-  M5.begin();
+  M5.begin();   //This line should be at the beginning of every program
 
-  M5.Lcd.print("Hello world!");
+  M5.Lcd.print("Hello world!");   //display "hello world!" on the LCD
 }
 
 void loop() {
