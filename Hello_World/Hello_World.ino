e07de402663698a15d4714de620881ee90daa651
Greg <garmstrong456@gmail.com>
2019-07-01 14:11:40 -0400

    updated hello world

--- Hello_World/Hello_World.ino
+++ Hello_World/Hello_World.ino
@@ -1,12 +1,36 @@
 #include <M5Stack.h>
+#include "FastLED.h"
+
+#define Neopixel_PIN 1
+#define NUM_LEDS 7
+CRGB leds[NUM_LEDS];
+
 
 void setup() {
   M5.begin();   //This line should be at the beginning of every program
 
-  M5.Lcd.print("Hello world!");   //display "hello world!" on the LCD
+  FastLED.addLeds<WS2811,Neopixel_PIN,RGB>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.clear();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  M5.Lcd.setTextSize(2);
+  M5.Lcd.setTextColor(BLUE, BLACK);
+  M5.Lcd.setCursor(0,0);
+  M5.Lcd.print("Hello world!");
+
+  fill_solid(leds, NUM_LEDS, CRGB::Blue);
+  FastLED.show();
+  
+  delay(500);
+
+  M5.Lcd.setTextColor(RED, BLACK);
+  M5.Lcd.setCursor(0,0);
+  M5.Lcd.print("Hello world!");
+
+  fill_solid(leds, NUM_LEDS, CRGB::Red);
+  FastLED.show();
+
+  delay(500);
 
 }
