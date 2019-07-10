c1e16cf51c3c24f67580aba8dafbdcba998f784d
Greg <garmstrong456@gmail.com>
2019-07-10 12:47:17 -0400

    cleaned up AccelStepper_drive comments

--- AccelStepper_drive/AccelStepper_drive.ino
+++ AccelStepper_drive/AccelStepper_drive.ino
@@ -1,22 +1,7 @@
-// For TEC Bot 7.0 with grbl 1.1 loaded on the Nano V3 co-processor
-// Grbl settings changed from default using the Universal Gcode sender as follows:
-// $100=32 -- For the x axis (left motor) set to 1/16th micro stepping, this makes 100 steps equal to 1 full rotation of the wheel
-// $101=32 -- Same, but for the Y axis (right motor)
-// $110=64000 -- Fastest stable tested speed for 1/16th micro stepping.  Can go faster by selecting different micro stepping (hardware switch)
-// $111=64000 -- Same for Y
-// $120=1000 -- Acceleratin that reliably prevents skipping steps...could push higher
-// $121=1000 -- Same for Y
-// 1/16th micro stepping is selected on the blue hardware switch by switching the 3rd switch on. (Off, Off, On)
+// For TEC Bot 7.0 with Greg Armstrong's AccelStepper_control firmware installed
 
-// This program sends instructions to grbl and is set with enough time between commands so that grbl doesn't get behind.
-// It is also possible to send much smaller step sizes in a loop.  When grble gets two back to back travel commands, it smoothly
-// accellerates to the new command...so smooth driving can be achieved easily instead of start stop start stop.
-
-// Grbl also keeps the x and y axis coordinated so they start and stop at the same time.  This makes it easy to do smooth turns for example.
-
-// The FastLED library is used instead of the Neopixel library because the Neopixel library conflicts with the m5Stack library somehow
-
-// This example makes the robot back up 4 revolutions and then drive forward quickly 4 revolutions...then repeat.
+//This program makes the robot drive forward quickly for 3 seconds, then back
+//up slowly for 6 seconds
 
 #include <M5Stack.h>
 #include "FastLED.h"
@@ -27,10 +12,8 @@
 CRGB leds[NUM_LEDS];
 
 //Motor definitions
-//Max dash speed is about 64000 ticks/min
 int dashSpeed = 20000; //medium
 int backingSpeed = 10000; //slow
-int driveDistance = 400; //positive numbers are FWD. 100 = 1 full revolution
 int pauseTime = 3000; //delay between the start of drive commands
 
 
@@ -45,9 +28,9 @@ void setup()
   M5.Lcd.setTextColor(WHITE);
   M5.Lcd.setCursor(0, 25);
   M5.Lcd.println();
-  M5.Lcd.println("- Drive Forward 4 revolutions");
+  M5.Lcd.println("- Dash Forward for 3 seconds");
   M5.Lcd.println();
-  M5.Lcd.println("- Back up 4 revolutions.");
+  M5.Lcd.println("- Back up for 6 seconds");
   M5.Lcd.println();
   M5.Lcd.println("- Change the LED colors.");
   M5.Lcd.println();
@@ -61,7 +44,7 @@ void setup()
   // Serial2.begin(unsigned long baud, uint32_t config, int8_t rxPin, int8_t txPin, bool invert)
   Serial2.begin(115200, SERIAL_8N1, 16, 17);
   
-  //give grbl time to start up on Nano
+  //give the firmware time to start up on Nano
   delay(1000); 
 }
 
@@ -72,9 +55,9 @@ void loop()
   FastLED.show();
 
   //Send the drive forward command
-  //driveDistance: 100 = 1 full rotation
-  //speed: units/minute (0 - 64000)
-  setDrive(10000, 10000, dashSpeed);
+  //drive speeds range from 0..64000
+  //64000 may be too fast for the motors to turn, depending on the surface
+  setDrive(10000, 10000);
 
   delay(pauseTime);
 
@@ -87,7 +70,7 @@ void loop()
   delay(2*pauseTime);
 }
 
-void setDrive(int Xdistance, int Ydistance, int rate) {
+void setDrive(int Xspeed, int Yspeed) {
   Serial2.printf("S1 %6d\r", Xdistance);
   Serial2.printf("S2 %6d\r", Ydistance);
 }
